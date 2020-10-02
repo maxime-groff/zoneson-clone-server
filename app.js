e889e9701066297da6640d51cffe4736f0f561d1
Maxime Groff <maxime.groff@engie.com>
2020-10-02 11:53:06 +0200

    feat: basic cors added

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const app = express()
 const connectDB = require('./config/db')
 connectDB()
 
+const PORT = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 5000 
+
+// middleware that ensures that cors errors are prevented
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // allow the access of the api, to all clients/websites asking
+  res.header('Access-Control-Allow-Headers', '*'); // which kind of header we want to accept 
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
